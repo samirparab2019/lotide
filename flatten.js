0cca9485f9e6594396989acec43b3fe10c7e7a79
Samir Parab <samirparab@yahoo.com>
2019-10-16 02:02:00 +0000

    modified.

---
+++ flatten.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+
+const assertArrayEquals = function(a, b) {
+  
+  if (a.length !== b.length) {
+  return false;
+  } else {
+  for (var i = 0; i < a.length; ++i) {
+    if (!assertEqual (a[i], b[i])) {
+      console.log("false");
+      return false;
+    }
+  }
+  console.log("true");
+  return true;
+}
+}
+
+function flatten(arr) {
+  return arr.reduce(function (flat, toFlatten) {
+    return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+  }, []);
+}
+ 
+
+
+
+
+console.log(flatten([1, 2, [3, 4], 5, [6]])) // => [1, 2, 3, 4, 5, 6];
