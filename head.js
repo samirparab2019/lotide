69ce1bccd1ee5f0ca75fe639dcc6a13886997582
Samir Parab <samirparab@yahoo.com>
2019-10-21 15:55:08 +0000

    head file modified to match new head test file.

--- head.js
+++ head.js
@@ -1,12 +1,3 @@
-// FUNCTION IMPLEMENTATION
-// //const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`${String.fromCodePoint(0x1F600)} Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`${String.fromCodePoint(0x1F34A)} Assertion Failed: ${actual} !=== ${expected}`);
-//   }
-// };
-
 
 const head = function(array) {
   return array[0];
