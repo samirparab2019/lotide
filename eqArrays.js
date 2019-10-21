b28ce8e97deb79bac0c7ef051c508f40246f30f5
Samir Parab <samirparab@yahoo.com>
2019-10-21 19:07:37 +0000

    eq arrays file modified for testing.

--- eqArrays.js
+++ eqArrays.js
@@ -1,31 +1,18 @@
+
+
 const eqArrays = function(a, b) {
   if (a.length !== b.length) {
     return false;
   } else {
     for (let i = 0; i < a.length; ++i) {
       if (a[i] !== b[i]) {
-        console.log("false");
+        //console.log("false");
         return false;
       }
     }
-    console.log("true");
+    //console.log("true");
     return true;
   }
 };
 
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`${String.fromCodePoint(0x1F600)} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${String.fromCodePoint(0x1F34A)} Assertion Failed: ${actual} !=== ${expected}`);
-  }
-
-};
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should pass
+module.exports = eqArrays;
