6cda9422830d73e507f022eb7d4afe0fb941cbbe
Samir Parab <samirparab@yahoo.com>
2019-10-21 20:14:33 +0000

    files modified.

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,32 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const middle = require('../middle');
+// const assertArraysEqual = require('../assertArraysEqual');
+// const middle = require('../middle');
 
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2] assertArrayEquals(middle([1,2,3]), [2])
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
+// assertArraysEqual(middle([1]), []); // => []
+// assertArraysEqual(middle([1, 2]), []); // => []
+// assertArraysEqual(middle([1, 2, 3]), [2]); // => [2] assertArrayEquals(middle([1,2,3]), [2])
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
+
+
+const assert = require('chai').assert;
+const middle   = require('../middle');
+
+describe("#middle", () => {
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]), ([]));
+  });
+
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), ([]));
+  });
+
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), ([2]));
+  });
+
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), ([2, 3]));
+  });
+
+}); 
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,15 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const tail = require('../tail');
 
-assertArraysEqual(tail([5,6,7]), [6, 7]);
-assertArraysEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+const assert = require('chai').assert;
+const tail   = require('../tail');
+
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), ([2, 3]));
+  });
+
+  it("returns [Lighthouse, Labs] for [Hello, Lighthouse, Labs]", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), (["Lighthouse", "Labs"]));
+  });
+
+});
+
